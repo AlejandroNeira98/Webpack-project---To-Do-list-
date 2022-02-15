0e0718403b18940f7bcb1689233110f7a5b37481
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-02-15 14:24:55 -0500

    Revert "Add array object and function"
    This reverts commit 81bae1ffe27a868c46e912399d540a1e73142c0a.

--- src/index.js
+++ src/index.js
@@ -11,25 +11,3 @@ function component() {
 }
 
 document.body.appendChild(component());
-
-const list = document.body.getElementsByClassName('ToDolist');
-const tasks = [];
-
-for (let i = 0 ; i < tasks.length ; i++) {
-  let li = document.createElement('div')
-  li.classList.add('li');
-  let taskToAdd = {};
-  tasks.forEach( task => {
-    if(task.index === i){
-      taskToAdd = task;
-      break;
-    }
-  } )
-  li.innerHTML = `
-    <input type="checkbox" id="${taskToAdd.index}" name="${taskToAdd.index}" checked="${taskToAdd.completed}" >
-    <label for="${taskToAdd.index}"> ${taskToAdd.description}</label>
-  `
-  list.appendChild(li)
-}
-
-
