d51ab559b044082cec812557dc95b315111b84e4
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-02-15 17:58:33 -0500

    Stilyng and Images

--- src/index.js
+++ src/index.js
@@ -1,33 +1,31 @@
 import _ from 'lodash';
 import './style.css';
-
-function component() {
-  const element = document.createElement('div');
-
-// Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+import more from './images/more.png';
 
 const list = document.body.querySelector('.ToDolist');
 
 const tasks = [
   {
-    index: 2,
-    description: "some task",
-    completed: true
-  },{
     index: 1,
+    description: "some task",
+    completed: "true"
+  },{
+    index: 2,
     description: "some task 2",
-    completed: false
+    completed: "false"
+  },{
+    index: 3,
+    description: "some task 3",
+    completed: "false"
+  },{
+    index: 4,
+    description: "some task 4",
+    completed: "false"
   }
 ];
 
 for (let i = 1 ; i <= tasks.length ; i++ ) {
-  let li = document.createElement('div')
+  let li = document.createElement('div');
   li.classList.add('li');
   let taskToAdd = {};
   tasks.forEach( task => {
@@ -36,11 +34,22 @@ for (let i = 1 ; i <= tasks.length ; i++ ) {
     }
   } )
   li.innerHTML = `
+    <span>
     <input type="checkbox" id="${taskToAdd.index}" name="${taskToAdd.index}" checked="${taskToAdd.completed}" >
     <label for="${taskToAdd.index}"> ${taskToAdd.description}</label>
-    <i></i>
+    </span>
   `
   list.appendChild(li);
 }
 
+const htmlTasks = document.getElementsByClassName('li');
 
+
+for (const task of htmlTasks) {
+  let icon = document.createElement('i');
+  const image = new Image();
+  image.src = more;
+  icon.appendChild(image);
+  task.appendChild(icon);
+  console.log(task);
+} 
