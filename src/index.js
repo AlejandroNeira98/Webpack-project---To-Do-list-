583ddd1a3782d9a58590fec2ee702bee155fed87
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-02-15 15:04:14 -0500

    update for loop that populates HTML

--- src/index.js
+++ src/index.js
@@ -12,24 +12,35 @@ function component() {
 
 document.body.appendChild(component());
 
-const list = document.body.getElementsByClassName('ToDolist');
-const tasks = [];
+const list = document.body.querySelector('.ToDolist');
 
-for (let i = 0 ; i < tasks.length ; i++) {
+const tasks = [
+  {
+    index: 2,
+    description: "some task",
+    completed: true
+  },{
+    index: 1,
+    description: "some task 2",
+    completed: false
+  }
+];
+
+for (let i = 1 ; i <= tasks.length ; i++ ) {
   let li = document.createElement('div')
   li.classList.add('li');
   let taskToAdd = {};
   tasks.forEach( task => {
     if(task.index === i){
       taskToAdd = task;
-      break;
     }
   } )
   li.innerHTML = `
     <input type="checkbox" id="${taskToAdd.index}" name="${taskToAdd.index}" checked="${taskToAdd.completed}" >
     <label for="${taskToAdd.index}"> ${taskToAdd.description}</label>
+    <i></i>
   `
-  list.appendChild(li)
+  list.appendChild(li);
 }
 
 
