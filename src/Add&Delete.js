e70fa95d9bad6f1e7e430e6b7513eb4bfbf94bb6
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-02-16 18:44:00 -0500

    Add functions in different module

---
+++ src/Add&Delete.js
@@ -0,0 +1,52 @@
+export function updateIndexes(tasks) {
+  const tasksOrdered = [];
+  for (let i = 1; i <= tasks.length; i += 1) {
+    let min = tasks[0];
+    for (let n = 0; n < tasks.length; n += 1) {
+      if (min.index >= tasks[n].index) {
+        min = tasks[n];
+      }
+    }
+    min.index = i;
+    tasksOrdered.push(min);
+    tasks.splice(tasks.indexOf(min), 1);
+  }
+  tasks = tasksOrdered;
+  if (tasksOrdered !== []) {
+    window.localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+}
+
+export function addTask(tasks) {
+  const inputTask = document.getElementById('addTask');
+  if (inputTask.value !== '') {
+    const inputTaskObj = {
+      index: tasks.length + 1,
+      description: inputTask.value,
+      completed: false,
+    };
+    tasks.push(inputTaskObj);
+    if (tasks[0].description !== '') { window.localStorage.setItem('tasks', JSON.stringify(tasks)); }
+  } 
+
+  
+  inputTask.value = null;
+  
+}
+
+export function removeTasks(tasks) {
+  function completed(object) { return object.completed === false; }
+  tasks = tasks.filter(completed);
+  /* update indexes */
+  updateIndexes(tasks);
+}
+
+export function removeTask(tasks) {
+  function completed(object) { return object.completed === false; }
+  tasks = tasks.filter(completed);
+}
+
+export function editTaskDescription(htmlTask, taskObj, tasks) {
+  taskObj.description = htmlTask.value;
+  window.localStorage.setItem('tasks', JSON.stringify(tasks));
+}
--- src/index.js
+++ src/index.js
@@ -1,29 +1,23 @@
 import './style.css';
 import more from './images/more.png';
+import {
+  addTask, removeTask, editTaskDescription, updateIndexes,
+} from './Add&Delete.js';
 
 const list = document.body.querySelector('.ToDolist');
 
-const tasks = [
-  {
-    index: 1,
-    description: 'some task',
-    completed: 'true',
-  }, {
-    index: 2,
-    description: 'some task 2',
-    completed: 'false',
-  }, {
-    index: 3,
-    description: 'some task 3',
-    completed: 'false',
-  }, {
-    index: 4,
-    description: 'some task 4',
-    completed: 'false',
-  },
-];
+let tasks = [];
+if (tasks !== []) { addTask(tasks); }
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+  removeTask(tasks); console.log(tasks);
+  updateIndexes(tasks);
+});
 
-function pupulateHTML() {
+function populateHTML(tasks) {
+  const lis = document.querySelectorAll('.li');
+  lis.forEach((li) => li.remove());
+
   for (let i = 1; i <= tasks.length; i += 1) {
     const li = document.createElement('div');
     li.classList.add('li');
@@ -34,12 +28,17 @@ function pupulateHTML() {
       }
     });
     li.innerHTML = `
-      <span>
-      <input type="checkbox" id="${taskToAdd.index}" name="${taskToAdd.index}" checked="${taskToAdd.completed}" >
-      <label for="${taskToAdd.index}"> ${taskToAdd.description}</label>
+      <span class="task">
+      <input type="checkbox"  name="${taskToAdd.index}" >
+      <input type="text" id="${taskToAdd.index}" value="${taskToAdd.description}">
       </span>
     `;
     list.appendChild(li);
+
+    const htmlTask = document.getElementById(`${taskToAdd.index}`);
+    htmlTask.addEventListener('keypress', () => {
+      editTaskDescription(htmlTask, taskToAdd, tasks);
+    });
   }
 
   const htmlTasks = document.getElementsByClassName('li');
@@ -53,4 +52,18 @@ function pupulateHTML() {
   }
 }
 
-window.addEventListener('load', pupulateHTML());
+window.addEventListener('load', () => {
+console.log(window.localStorage.getItem('tasks'));
+  if (window.localStorage.getItem('tasks') !== null) {
+    tasks = JSON.parse(window.localStorage.getItem('tasks'));
+    populateHTML(tasks);
+  }
+});
+
+const inputTask = document.getElementById('addTask');
+inputTask.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter' && inputTask.value !== '') {
+    addTask(tasks);
+    populateHTML(tasks);
+  }
+});
